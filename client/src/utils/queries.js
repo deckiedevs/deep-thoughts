295aa631d15b9c5eed2cbf2156cc009dc638e95a
Mila Decker <milacdecker@gmail.com>
2021-03-24 18:30:01 -0400

    built single thought page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const QUERY_THOUGHTS = gql`
-query thoughts($username: String) {
+    query thoughts($username: String) {
     thoughts(username: $username) {
         _id
         thoughtText
@@ -15,4 +15,22 @@ query thoughts($username: String) {
         }
     }
 }
+`;
+
+export const QUERY_THOUGHT = gql`
+    query thought($id: ID!) {
+        thought(_id: $id) {
+            _id
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
 `;
